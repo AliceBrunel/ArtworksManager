93284612412a8b2f64ed5963b70ef52c46ab6ebb
Alice Brunel <alc.brunel@gmail.com>
2020-01-19 19:21:19 +0000

    Updated migration file for Museum

--- db/migrate/20200119185103_create_museums.rb
+++ db/migrate/20200119185103_create_museums.rb
@@ -1,6 +1,10 @@
 class CreateMuseums < ActiveRecord::Migration[6.0]
   def change
     create_table :museums do |t|
+		t.string :name
+		t.string :location
+		t.string :specialty
+		t.belongs_to :user
 
       t.timestamps
     end
