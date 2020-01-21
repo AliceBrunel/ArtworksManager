f91693db2f91420a60696729238691aa70876947
Alice Brunel <alc.brunel@gmail.com>
2020-01-21 12:15:44 +0000

    Implemented the new, create and show action in the controller to test the user susbcription flow.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
-end
+
+  def index
+  end 
+
+  def new
+   @user = User.new
+  end 
+
+  def create 
+   if (user = User.create user_params)
+    redirect_to user_path(user)
+   else
+    render 'new'
+   end 
+  end 
+
+
+  def show 
+   @user = User.find_by(id:params[:id])
+  end
+
+ private 
+
+   def user_params 
+     params.require(:user).permit(
+	  :name,
+	  :surname,
+	  :email,
+	  :address,
+	  :phone,
+	  :position,
+	  :job
+      )
+   end
+
+ end
