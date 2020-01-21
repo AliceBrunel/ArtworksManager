c3ab4cfc354ca9896771b4d75e8ba00f223576c1
Alice Brunel <alc.brunel@gmail.com>
2020-01-21 13:24:38 +0000

    Created the form for Artwork, so that the user can add an artwork to its collection. But this need authentication first, to make sure the user_id is connected with the session so that the user_id is send as a parameter

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,7 +1,33 @@
 class ArtworksController < ApplicationController
 	
-	def index
-		@artworks = Artwork.all
-	end
+  def index
+   @artworks = Artwork.all
+  end
 
+  def new 
+   @artwork = Artwork.new
+  end 
+
+  def create
+   if (artwork = Artwork.create artwork_params)
+    redirect_to artwork_path(artwork)
+   else
+    render 'new'
+   end 
+  end 
+
+  def show 
+   @artwork = Artwork.find_by(id:params[:id])
+  end 
+
+ private 
+
+   def artwork_params 
+     params.require(:artwork).permit(
+	  :title,
+	  :price,
+	  :technic,
+	  :user_id
+      )
+   end
 end
