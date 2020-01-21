c48c744dcd0df7e0133c177f3b18e888f252f095
Alice Brunel <alc.brunel@gmail.com>
2020-01-21 11:50:08 +0000

    Erased the validation, as I am trying to verify the user stories first. Validations would require the user to have a working authenticating session, which I prefer to implement one the user process is verified.

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -2,7 +2,5 @@ class Artwork < ApplicationRecord
 	belongs_to :artist
 	belongs_to :user
 
-	validates_presence_of :title, :artist, :user
-
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,4 @@ class User < ApplicationRecord
 	has_many :museums, through :teams
 	has_many :artworks
 
-	validates_presence_of :name, :surname, :email, :job
 end
