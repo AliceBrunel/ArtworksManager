c65638c14f14ad906961b2e6aff3962c7952be80
Alice Brunel <alc.brunel@gmail.com>
2020-01-22 18:58:11 +0000

    Trying to implement the creation of artwork through artist. Created the artists controller, actions and associated views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,41 @@
 class ArtistsController < ApplicationController
+  def index
+   @artists = Artist.all
+  end
+
+  def new 
+   @artist = Artist.new
+   @artist.artworks.build
+  end 
+
+  def create
+   @artist = Artist.create(artist_params)
+   @artwork = @artist.artworks.last
+   @artwork.user = current_user
+   @artist.save
+   redirect_to artist_path(artist_url)
+  end 
+
+  def show 
+   @artist = Artist.find_by(id:params[:id])
+  end 
+
+ private 
+
+	def artist_params 
+		params.require(:artist).permit(
+			:full_name,
+			:biography,
+			:birth_date,
+			:death_date,
+			:style,
+			artworks_attributes: [
+				:user_id,
+				:title,
+				:price,
+				:technic
+				]
+		)
+	end
+
 end
