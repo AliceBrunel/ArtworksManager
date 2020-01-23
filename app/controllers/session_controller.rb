66c0295e3d6f36d5d971f25ccc44b56581b57786
Alice Brunel <alc.brunel@gmail.com>
2020-01-23 18:38:45 +0000

    The redirection to the user path wasn't working properly. Fixed the redirection, added a password field in the signin form. The session isn't working now, we will have to investigate.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,11 +2,13 @@ class SessionController < ApplicationController
 skip_before_action :verify_user_is_authenticated, only: [:new, :create]
 
   def new
-    @group_options = Group.all.map{|group| group.name }
+    @groups = Group.all
     @user = User.new
   end
 
   def create
+      @groups = Group.all
+
     if @user = User.find_by_email(params[:email])
       session[:user_id] = @user.id
 
