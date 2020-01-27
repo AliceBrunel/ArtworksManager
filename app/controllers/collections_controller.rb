b6224f5d817ad927cf4572b50a854886821aeddc
Alice Brunel <alc.brunel@gmail.com>
2020-01-27 12:02:22 +0000

    Created the actions new, create, show, edit for Collections and its collection parameters method

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,2 +1,30 @@
 class CollectionsController < ApplicationController
+
+	def new 
+		@collection = Collection.new
+	end
+
+	def create 
+		collection = Collection.create(collection_params)
+	end 
+
+	def show 
+		@collection = Collection.find_by(id:params[:id])
+	end 
+
+	def edit 
+		@collection = Collection.find_by(id:params[:id])
+	end 
+
+	private 
+
+		def collection_params 
+		params.require(:collection).permit(
+			:name, 
+			:description,
+			:start_date,
+			:end_date
+			)
+	end
+
 end
