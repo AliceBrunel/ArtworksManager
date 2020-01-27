12069d49578d1536846b1c4fd5265b0666882c0a
Alice Brunel <alc.brunel@gmail.com>
2020-01-27 11:57:47 +0000

    Created the actions for the museum lifecycle, and related views and forms

--- app/controllers/museums_controller.rb
+++ app/controllers/museums_controller.rb
@@ -2,4 +2,31 @@ class MuseumsController < ApplicationController
 	def index 
 		@museums = Museum.all
 	end
+
+	def new 
+		@museum = Museum.new
+	end 
+
+	def create 
+		museum = Museum.create(museum_params)
+		redirect_to museum_path(museum)
+	end
+
+	def show 
+		@museum = Museum.find_by(id:params[:id])
+	end
+
+	def edit 
+		@museum = Museum.find_by(id:params[:id])
+	end
+	
+	private 
+
+		def museum_params 
+			params.require(:museum).permit(
+				:name, 
+				:location,
+				:specialty
+			)
+		end
 end
